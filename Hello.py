667c7a5998440db1aa4bc8f85b429a2eade3dedf
YoosefYA <yalabri26@uwcchina.org>
2026-09-04 13:31:41 +0800

    added an ending message

--- Hello.py
+++ Hello.py
@@ -1,3 +1,8 @@
-print("hello world")
-x = 17
-print(x)
+is_student =str(input("Are you a student? (yes/no):"))
+#comment on nothing cuz why not -_-
+if is_student == "yes":
+    print("Welcome to UWC CSC!")
+else:
+    print("No worries! you can still explore the school!!")
+
+print("Thank you for trying this application!")
