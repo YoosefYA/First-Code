813d5ca6e7aa8e5e38552b6fe4ea022a021d0a5f
YoosefYA <yalabri26@uwcchina.org>
2026-09-11 14:15:25 +0800

    new shizea

---
+++ logicTesting.py
@@ -0,0 +1,11 @@
+temp = int(input("Enter the temperature: "))
+
+if temp > 0 and temp < 30:
+    print("The temperature is good")
+else:
+    print("The temperature is bad")
+
+if temp >=0 or temp <= 30:
+    print("The temperature is good")
+else:
+    print("The temperature is bad")
