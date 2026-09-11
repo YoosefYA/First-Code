f42af0cb37250837eedb6627d09b7ab862bd7ec4
YoosefYA <yalabri26@uwcchina.org>
2026-09-11 14:23:59 +0800

    random stuff

---
+++ conditionalExpressionsElifs.py
@@ -0,0 +1,3 @@
+num = int(input("Input a number: "))
+print("That number is positive" if num > 0 else "That number is a negative")
+print("That number is even" if num % 2 == 0 else "That number is odd")
