11451127d203cc06fa5affe05d23feeca3c1e822
YoosefYA <yalabri26@uwcchina.org>
2026-09-24 08:41:06 +0800

    random

--- newStringFunctions.py
+++ newStringFunctions.py
@@ -10,3 +10,8 @@ print(middle)
 p = "Banana"
 p = p.replace("a","o")
 print(p)
+
+email = input("Please enter your email: ")
+goodindex = email.find("@")
+print(f"Your email username is {email[:goodindex]}")
+print(f"Your domain is {email[goodindex + 1 :]}")
